b019d91242a065ceeaff1c18780216437903394f
Sergii Karastoyanov <karastoyanov13@gmail.com>
2020-11-19 17:51:03 +0200

    HT6-1

--- HomeTaskJS/CLASS6/task6-1.js
+++ HomeTaskJS/CLASS6/task6-1.js
@@ -1,27 +1,73 @@
 // =============КЛАССНАЯ РАБОТА=================
 // =============================================
 // =============================================
-//
-//
-//
-//
-// let cars = [ {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400}, {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250}, {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300}, {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140}, {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200}, {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165}, {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120}, {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120}, {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350}, {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180}, {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180}, {producer:"mercedes",model: "e63",year 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400}, {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230} ];
-// Відфільтрувати масив за наступними крітеріями :
-//     - двигун більше 3х літрів
-// - двигун = 2л
-// - виробник мерс
-// - двигун більше 3х літрів + виробник мерседес
-// - двигун більше 3х літрів + виробник субару
-// - сили більше ніж 300
-// - сили більше ніж 300 + виробник субару
-// - мотор серіі ej
-// - сили більше ніж 300 + виробник субару + мотор серіі ej
-// - двигун меньше 3х літрів + виробник мерседес
-// - двигун більше 2л + сили більше 250
-// - сили більше 250  + виробник бмв
-//
-//
-//
+
+
+let cars = [ {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400},
+    {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250},
+    {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300},
+    {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140},
+    {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200},
+    {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165},
+    {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120},
+    {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120},
+    {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350},
+    {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180},
+    {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180},
+    {producer:"mercedes",model: "e63",year: 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400},
+    {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230} ];
+
+/*Відфільтрувати масив за наступними крітеріями :*/
+/* - двигун більше 3х літрів*/
+// let a= cars.filter(car=>( car.volume > 3))
+// console.log(a)
+
+/*- двигун = 2л*/
+// let b= cars.filter(car=>car.volume == 2)
+// console.log(b)
+
+/*- виробник мерс */
+// let b= cars.filter(car=>car.producer ==='mercedes')
+// console.log(b)
+
+/* - двигун більше 3х літрів + виробник мерседес*/
+// let b= cars.filter(car=> {car.producer ==='mercedes'&& car.volume>3 })
+// console.log(b)
+
+/* - двигун більше 3х літрів + виробник субару */
+// let b= cars.filter(car=>{ car.producer ==='subaru'&& car.volume>3})
+// console.log(b)
+
+/*- сили більше ніж 300 */
+// let b= cars.filter(car=>{car.power>300})
+// console.log(b)
+
+/* - сили більше ніж 300 + виробник субару*/
+// let b= cars.filter(car=>{car.power>300 && car.producer==='subaru'})
+// console.log(b)
+
+/*- мотор серіі ej */
+// let b= cars.filter(car=>car.engine.startsWith('ej'))
+// console.log(b)
+
+/* - сили більше ніж 300 + виробник субару + мотор серіі ej */
+// let b= cars.filter(car => {car.power>300 && car.producer === 'subaru' && car.engine.startsWith('ej')})
+// console.log(b)
+
+/* - двигун меньше 3х літрів + виробник мерседес */
+let b= cars.filter(car => {car.volume< 3 && car.producer === 'mercedes'})
+console.log(b)
+
+/* - двигун більше 2л + сили більше 250 */
+// let b= cars.filter(car => {car.volume> 2 && car.power > 250})
+// console.log(b)
+
+/* - сили більше 250  + виробник бмв */
+// let b= cars.filter(car => {car.power> 250 && car.producer === 'bmw'})
+// console.log(b)
+
+
+
 // - взять слдующий массив
 // let usersWithAddress = [{id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}}, {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 1}}, {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}}, {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 90}}, {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}}, {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}}, {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 22}}, {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}}, {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 12}}, {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}}, {id: 11, name: 'max', age: 31, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}}];
 // -- отсортировать его по id пользователей
